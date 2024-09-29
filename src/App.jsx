6d351599835d56e36af681ca3d99443dd0f927bc
aburahan8721 <aburaihan8721@gmail.com>
2024-09-29 15:14:00 +0600

    only ui done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./index.css";
+// import Register from "./pages/register/Register";
+// import Profile from "./pages/profile/Profile";
+// import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+const App = () => {
+  return (
+    <div>
+      <Home />
+      {/* <Login /> */}
+      {/* <Profile /> */}
+      {/* <Register /> */}
+    </div>
+  );
+};
+
+export default App;
